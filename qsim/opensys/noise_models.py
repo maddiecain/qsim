93ff2fa7c374776fbccd3f6adebe2d350e98bf3c
Leo Zhou <leologist@gmail.com>
2019-10-21 23:48:23 -0400

    Added generalized Pauli channel; fixed bug in unit test

--- qsim/opensys/noise_models.py
+++ qsim/opensys/noise_models.py
@@ -1,4 +1,5 @@
 import qsim.ops as qops
+from typing import Tuple
 
 def depolarize_one_qubit(rho, i: int, p: float):
     """ Perform depolarizing channel on the i-th qubit of an input density matrix
@@ -8,10 +9,27 @@ def depolarize_one_qubit(rho, i: int, p: float):
             i = zero-based index of qubit location to apply pauli
             p = probability of depolarization, between 0 and 1
     """
-    
-    assert len(rho.shape) == 2 # rho should be a matrix
-    
     return (1-p)*rho + p/3*(qops.multiply_single_qubit_mixed(rho, i, 1) 
                           + qops.multiply_single_qubit_mixed(rho, i, 2)
                           + qops.multiply_single_qubit_mixed(rho, i, 3))
 
+
+def pauli_channel_one_qubit(rho, i: int, ps: Tuple[float]):
+    """ Perform general Pauli channel on the i-th qubit of an input density matrix
+
+        Input:
+            rho = input density matrix (as numpy.ndarray)
+            i = zero-based index of qubit location to apply pauli
+            ps = a 3-tuple (px, py, pz) indicating the probability
+                 of applying each Pauli operator
+
+        Returns:
+            (1-px-py-pz) * rho + px * Xi * rho * Xi
+                               + py * Yi * rho * Yi
+                               + pz * Zi * rho * Zi
+    """
+    assert len(ps) == 3
+
+    return (1-sum(ps))*rho + ( ps[0] * qops.multiply_single_qubit_mixed(rho, i, 1) 
+                             + ps[1] * qops.multiply_single_qubit_mixed(rho, i, 2)
+                             + ps[2] * qops.multiply_single_qubit_mixed(rho, i, 3))
--- qsim/opensys/noise_models_test.py
+++ qsim/opensys/noise_models_test.py
@@ -1,12 +1,13 @@
 from qsim.opensys import noise_models
 import qsim.ops as qops
+import numpy as np
 
 def test_depolarize_one_qubit():
     SX, SY, SZ = qops.PAULI_MATRICES
     op1 = np.kron(SX, SY)
     p = 0.093
-    op2 = nm.depolarize_one_qubit(op1, 1, p)
-    op3 = nm.depolarize_one_qubit(op2, 0, 2*p)
+    op2 = noise_models.depolarize_one_qubit(op1, 1, p)
+    op3 = noise_models.depolarize_one_qubit(op2, 0, 2*p)
     assert np.linalg.norm(op2 - op1*(1-4*p/3)) <= 1e-10
     assert np.linalg.norm(op3 - op1*(1-4*p/3)*(1-8*p/3)) <= 1e-10
     
