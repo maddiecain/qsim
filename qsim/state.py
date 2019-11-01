fixed bug in qubit rotation

qsim/state.py
@@ -7,7 +7,7 @@ class State(object):
     """
     def __init__(self, state, N, is_ket = True):
         # Cast into complex type
-        self.state = state.astype(np.complex64, copy = False)
+        self.state = state.astype(np.complex128, copy = False)
         self.is_ket = is_ket
         self.N = N
 
@@ -98,7 +98,7 @@ class State(object):
                 angle = rotation angle
                 op = unitary pauli operator or basis pauli index
         """
-        rot = np.array([[np.cos(angle), 0], [np.cos(angle)]]) + op * np.sin(angle)
+        rot = np.array([[np.cos(angle), 0], [0, np.cos(angle)]]) - op * 1j * np.sin(angle)
         self.single_qubit_operation(i, rot, is_pauli=False)
 
     def all_qubit_rotation(self, angle: float, op):
@@ -141,7 +141,7 @@ class State(object):
             n = eigenvectors.shape[0]
             outcomes = np.matmul(np.reshape(eigenvectors.conj(), (n, n, 1)), np.reshape(eigenvectors, (n, 1, n)))@state
             probs = np.trace(outcomes, axis1 = -2, axis2 = -1)
-            i = np.random.choice(operator.shape[0], p=probs)
+            i = np.random.choice(operator.shape[0], p=np.absolute(probs))
             return eigenvalues[i], outcomes[i]/probs
 
 

qsim/test/test_state.py
@@ -69,17 +69,16 @@ class TestState(unittest.TestCase):
         psi0[0] = 1
         state0 = State(psi0, 6, is_ket = True)
         state1 = State(psi0, 6, is_ket = True)
+
         # Rotate by exp(-1i*pi/4*sigma_y) every qubit to get |++++++>
-
         for i in range(state0.N):
-            state0.single_qubit_rotation(i, np.pi / 4, 2)
-
+            state0.single_qubit_rotation(i, np.pi/4, tools.SY)
         self.assertAlmostEqual(np.vdot(state0.state, np.ones(2**state0.N)/2**(state0.N/2)), 1)
 
         # Apply exp(-1i*pi/4*sigma_x)*exp(-1i*pi/4*sigma_z) on every qubit to get exp(-1j*N*pi/4)*|000000>
         for i in range(state0.N):
-            state0.single_qubit_rotation(i, np.pi / 4, 3)
-            state0.single_qubit_rotation(i, np.pi / 4, 1)
+            state0.single_qubit_rotation(i, np.pi / 4, tools.SZ)
+            state0.single_qubit_rotation(i, np.pi / 4, tools.SX)
 
         self.assertTrue(np.abs(np.vdot(state1.state, state0.state)*np.exp(1j*np.pi/4*state1.N) - 1) <= 1e-10)
 