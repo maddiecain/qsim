- Sped up Rydberg Hamiltonian generation - Improved scaling runtime on driver Hamiltonian generation - Bugfix in generation of number of integrator steps - Script for analyzing scaling in 1D

qsim/graph_algorithms/adiabatic.py
@@ -87,9 +87,8 @@ class SimulateAdiabatic(object):
 
     def run(self, time, schedule, num=None, initial_state=None, full_output=True, method='RK45', verbose=False,
             iterations=None):
-        if method == 'odeint' or method == 'trotterize' and num is None:
+        if (method == 'odeint' or method == 'trotterize') and num is None:
             num = self._num_from_time(time, method=method)
-
         if initial_state is None:
             # Begin with all qudits in the ground s
             initial_state = State(np.zeros((self.cost_hamiltonian.hamiltonian.shape[0], 1)), code=self.code,

qsim/graph_algorithms/graph.py
@@ -5,15 +5,17 @@ import matplotlib.pyplot as plt
 import networkx as nx
 from networkx.algorithms import approximation
 from itertools import tee, product
+from collections import deque
+from itertools import chain, islice
 
 """Class for performing basic graph Monte Carlo operations on networkx Graphs. Future versions of this code
 may subclass nx.Graph."""
 
 
 class Graph(object):
-    def __init__(self, graph: nx.Graph, IS=True):
+    def __init__(self, graph: nx.Graph, IS=True, generate_mixer=False):
         # Set default weights to one
-
+        self.generate_mixer = generate_mixer
         for edge in graph.edges:
             if not ('weight' in graph.edges[edge]):
                 graph.edges[edge]['weight'] = 1
@@ -37,13 +39,152 @@ class Graph(object):
         if IS:
             self.generate_independent_sets()
 
+    def enumerate_all_cliques(self, complement):
+        """Returns all cliques in an undirected graph.
+
+        This method returns cliques of size (cardinality)
+        k = 1, 2, 3, ..., maxDegree - 1.
+
+        Where maxDegree is the maximal degree of any node in the graph.
+
+        Parameters
+        ----------
+        G: undirected graph
+
+        Returns
+        -------
+        generator of lists: generator of list for each clique.
+
+        Notes
+        -----
+        To obtain a list of all cliques, use
+        :samp:`list(enumerate_all_cliques(G))`.
+
+        Based on the algorithm published by Zhang et al. (2005) [1]_
+        and adapted to output all cliques discovered.
+
+        This algorithm is not applicable on directed graphs.
+
+        This algorithm ignores self-loops and parallel edges as
+        clique is not conventionally defined with such edges.
+
+        There are often many cliques in graphs.
+        This algorithm however, hopefully, does not run out of memory
+        since it only keeps candidate sublists in memory and
+        continuously removes exhausted sublists.
+
+        References
+        ----------
+        .. [1] Yun Zhang, Abu-Khzam, F.N., Baldwin, N.E., Chesler, E.J.,
+               Langston, M.A., Samatova, N.F.,
+               Genome-Scale Computational Approaches to Memory-Intensive
+               Applications in Systems Biology.
+               Supercomputing, 2005. Proceedings of the ACM/IEEE SC 2005
+               Conference, pp. 12, 12-18 Nov. 2005.
+               doi: 10.1109/SC.2005.29.
+               http://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=1559964&isnumber=33129
+        """
+        index = {}
+        nbrs = {}
+        for u in complement:
+            index[u] = len(index)
+            # Neighbors of u that appear after u in the iteration order of G.
+            nbrs[u] = {v for v in complement[u] if v not in index}
+        queue = deque(([u], sorted(nbrs[u], key=index.__getitem__)) for u in complement)
+        # Loop invariants:
+        # 1. len(base) is nondecreasing.
+        # 2. (base + cnbrs) is sorted with respect to the iteration order of G.
+        # 3. cnbrs is a set of common neighbors of nodes in base.
+        while queue:
+            base, cnbrs = map(list, queue.popleft())
+            yield base
+            for i, u in enumerate(cnbrs):
+                # Use generators to reduce memory consumption.
+                queue.append((chain(base, [u]), filter(nbrs[u].__contains__, islice(cnbrs, i + 1, None))))
+
     def generate_independent_sets(self):
+        def return_neighbors(complement):
+            """Returns all cliques in an undirected graph.
+
+            This method returns cliques of size (cardinality)
+            k = 1, 2, 3, ..., maxDegree - 1.
+
+            Where maxDegree is the maximal degree of any node in the graph.
+
+            Parameters
+            ----------
+            G: undirected graph
+
+            Returns
+            -------
+            generator of lists: generator of list for each clique.
+
+            Notes
+            -----
+            To obtain a list of all cliques, use
+            :samp:`list(enumerate_all_cliques(G))`.
+
+            Based on the algorithm published by Zhang et al. (2005) [1]_
+            and adapted to output all cliques discovered.
+
+            This algorithm is not applicable on directed graphs.
+
+            This algorithm ignores self-loops and parallel edges as
+            clique is not conventionally defined with such edges.
+
+            There are often many cliques in graphs.
+            This algorithm however, hopefully, does not run out of memory
+            since it only keeps candidate sublists in memory and
+            continuously removes exhausted sublists.
+
+            References
+            ----------
+            .. [1] Yun Zhang, Abu-Khzam, F.N., Baldwin, N.E., Chesler, E.J.,
+                   Langston, M.A., Samatova, N.F.,
+                   Genome-Scale Computational Approaches to Memory-Intensive
+                   Applications in Systems Biology.
+                   Supercomputing, 2005. Proceedings of the ACM/IEEE SC 2005
+                   Conference, pp. 12, 12-18 Nov. 2005.
+                   doi: 10.1109/SC.2005.29.
+                   http://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=1559964&isnumber=33129
+            """
+            index = {}
+            nbrs = {}
+            num = 0
+            all_nbrs = {}
+            for u in complement:
+                index[u] = len(index)
+                # Neighbors of u that appear after u in the iteration order of G.
+                nbrs[u] = {v for v in complement[u]}  # if v not in index}
+                all_nbrs[u] = {v for v in complement[u]}
+            queue = deque(([u], sorted(nbrs[u], key=index.__getitem__)) for u in complement)
+            # Loop invariants:
+            # 1. len(base) is nondecreasing.
+            # 2. (base + cnbrs) is sorted with respect to the iteration order of G.
+            # 3. cnbrs is a set of common neighbors of nodes in base.
+            num = 0
+            print(queue)
+            while queue:
+                base, cnbrs = map(list, queue.popleft())
+                print(base, cnbrs)
+                if all(base[i] <= base[i + 1] for i in range(len(base) - 1)):
+                    yield base
+                    num += 1
+                    for i, u in enumerate(cnbrs):
+                        # print(nbrs[u])
+                        # Use generators to reduce memory consumption.
+                        # print(all_nbrs[u])
+                        queue.append((chain(base, [u]), filter(nbrs[u].__contains__, islice(cnbrs, i + 1, None))))
+                else:
+                    print('hi')
+            print(num)
+
         # Construct generator containing independent sets
         # Don't generate anything that depends on the entire Hilbert space as to save space
         # Generate complement graph
         complement = nx.complement(self.graph)
         # These are your independent sets of the original graphs, ordered by node and size
-        independent_sets, backup = tee(nx.algorithms.clique.enumerate_all_cliques(complement))
+        independent_sets, backup = tee(self.enumerate_all_cliques(complement))
         self.num_independent_sets = sum(1 for _ in backup) + 1  # We add one to include the empty set
         # Generate a list of integers corresponding to the independent sets in binary
         indices = np.zeros((self.num_independent_sets, self.n), dtype=int)
@@ -51,8 +192,15 @@ class Graph(object):
         indices[-1, ...] = np.ones(self.n)
         k = self.num_independent_sets - 2
         self.mis_size = 0
-        # All spins down should be at the end
+        if self.generate_mixer:
+            independent_sets_dict = {(): (self.num_independent_sets-1, [])}
         for i in independent_sets:
+            if self.generate_mixer:
+                independent_sets_dict[tuple(i)] = (k, [])
+                for (j, node) in enumerate(i):
+                    i_removed = i.copy()
+                    i_removed.pop(j)
+                    independent_sets_dict[tuple(i_removed)][1].append(k)
             nary = np.ones(self.n)
             for node in i:
                 nary[node] = 0
@@ -60,6 +208,56 @@ class Graph(object):
             if len(i) > self.mis_size:
                 self.mis_size = len(i)
             k -= 1
+        if self.generate_mixer:
+            self.independent_sets_dict = independent_sets_dict
+        # All spins down should be at the end
+
+        """for i in range(num_IS):
+            for j in range(graph.n):
+                if IS[i, j] == self.transition[0]:
+                    # Flip spin at this location
+                    # Get binary representation
+                    temp = IS[i, ...].copy()
+                    temp[j] = self.transition[1]
+                    where_matched = (np.argwhere(np.sum(np.abs(IS - temp), axis=1) == 0).flatten())
+                    if len(where_matched) > 0:
+                        # This is a valid spin flip
+                        rows[num_terms] = where_matched[0]
+                        columns[num_terms] = i
+                        if self.pauli == 'X':
+                            entries[num_terms] = 1
+                        elif self.pauli == 'Y':
+                            entries[num_terms] = -1j
+                        num_terms += 1
+        # Cut off the excess in the arrays
+        columns = columns[:2 * num_terms]
+        rows = rows[:2 * num_terms]
+        entries = entries[:2 * num_terms]
+        # Populate the second half of the entries according to self.pauli
+        if self.pauli == 'X':
+            columns[num_terms:2 * num_terms] = rows[:num_terms]
+            rows[num_terms:2 * num_terms] = columns[:num_terms]
+            entries[num_terms:2 * num_terms] = entries[:num_terms]
+        elif self.pauli == 'Y':
+            columns[num_terms:2 * num_terms] = rows[:num_terms]
+            rows[num_terms:2 * num_terms] = columns[:num_terms]
+            entries[num_terms:2 * num_terms] = -1 * entries[:num_terms]
+        # Now, construct the Hamiltonian
+        self._csc_hamiltonian = sparse.csc_matrix((entries, (rows, columns)), shape=(num_IS, num_IS))
+        self._hamiltonian = self._csc_hamiltonian"""
+        """rows = np.zeros(self.n * self.num_independent_sets, dtype=int)
+        columns = np.zeros(self.n * self.num_independent_sets, dtype=int)
+        entries = np.zeros(self.n * self.num_independent_sets, dtype=int)
+        num_terms = 0
+        for (i, nbrs) in independent_sets:
+            nary = np.ones(self.n)
+            for node in i:
+                nary[node] = 0
+            indices[k, ...] = nary
+            if len(i) > self.mis_size:
+                self.mis_size = len(i)
+            k -= 1"""
+
         self.degeneracy = len(np.argwhere(self.n - np.sum(indices, axis=1) == self.mis_size))
         self.independent_sets = indices
         return
@@ -261,7 +459,7 @@ class GraphMonteCarlo(object):
         return nx.algorithms.approximation.maximum_independent_set(self.graph)
 
 
-def line_graph(n, IS=True):
+def line_graph(n, IS=True, generate_mixer=False):
     g = nx.Graph()
     g.add_nodes_from(np.arange(0, n), weight=1)
     if n == 1:
@@ -269,10 +467,10 @@ def line_graph(n, IS=True):
     else:
         for i in range(n - 1):
             g.add_edge(i, i + 1, weight=1)
-    return Graph(g, IS=IS)
+    return Graph(g, IS=IS, generate_mixer=generate_mixer)
 
 
-def ring_graph(n, node_weight=1, edge_weight=1, IS=True):
+def ring_graph(n, node_weight=1, edge_weight=1, IS=True, generate_mixer=False):
     g = nx.Graph()
     g.add_nodes_from(np.arange(0, n), weight=node_weight)
     if n == 1:
@@ -281,14 +479,14 @@ def ring_graph(n, node_weight=1, edge_weight=1, IS=True):
         for i in range(n - 1):
             g.add_edge(i, i + 1, weight=edge_weight)
         g.add_edge(0, n - 1, weight=edge_weight)
-    return Graph(g, IS=IS)
+    return Graph(g, IS=IS, generate_mixer=generate_mixer)
 
 
-def degree_fails_graph(return_mis=False, IS=True):
+def degree_fails_graph(return_mis=False, IS=True, generate_mixer=False):
     g = nx.Graph()
     g.add_weighted_edges_from(
         [(0, 1, 1), (0, 4, 1), (0, 5, 1), (4, 5, 1), (1, 4, 1), (1, 3, 1), (2, 4, 1)])
-    return Graph(g, IS=IS)
+    return Graph(g, IS=IS, generate_mixer=generate_mixer)
 
 
 def IS_projector(graph, code):
@@ -326,7 +524,7 @@ def IS_projector(graph, code):
         return np.array([np.diagonal(proj)]).T
 
 
-def grid_graph(n, m, periodic=False, nn=False, IS=True):
+def grid_graph(n, m, periodic=False, nn=False, IS=True, generate_mixer=False):
     graph = nx.grid_2d_graph(n, m, periodic=periodic)
     nodes = graph.nodes
     if nn:
@@ -343,10 +541,11 @@ def grid_graph(n, m, periodic=False, nn=False, IS=True):
     new_nodes = list(range(len(nodes)))
     mapping = dict(zip(nodes, new_nodes))
     nx.relabel_nodes(graph, mapping, copy=False)
-    return Graph(graph, IS=IS)
+    return Graph(graph, IS=IS, generate_mixer=generate_mixer)
 
 
-def unit_disk_grid_graph(grid, radius=np.sqrt(2) + 1e-5, visualize=False, periodic=False, IS=True):
+def unit_disk_grid_graph(grid, radius=np.sqrt(2) + 1e-5, visualize=False, periodic=False, IS=True,
+                         generate_mixer=False):
     x = grid.shape[1]
     y = grid.shape[0]
 
@@ -392,14 +591,15 @@ def unit_disk_grid_graph(grid, radius=np.sqrt(2) + 1e-5, visualize=False, period
         nx.draw_networkx_edges(g, pos=pos, edge_color='black')
         plt.axis('off')
         plt.show()
-    g = Graph(g, IS=IS)
+    g = Graph(g, IS=IS, generate_mixer=generate_mixer)
     g.positions = grid
     g.radius = radius
     g.periodic = periodic
     return g
 
 
-def unit_disk_grid_graph_rydberg(grid, radius=np.sqrt(2) + 1e-5, B=863300 / (4.47) ** 6, visualize=False, IS=True):
+def unit_disk_grid_graph_rydberg(grid, radius=np.sqrt(2) + 1e-5, B=863300 / (4.47) ** 6, visualize=False, IS=True,
+                                 generate_mixer=False):
     y, x = grid.shape
 
     def neighbors_from_geometry(n):
@@ -441,7 +641,7 @@ def unit_disk_grid_graph_rydberg(grid, radius=np.sqrt(2) + 1e-5, B=863300 / (4.4
         nx.draw_networkx_edges(g, pos=pos, edge_color='black')
         plt.axis('off')
         plt.show()
-    g = Graph(g, IS=IS)
+    g = Graph(g, IS=IS, generate_mixer=generate_mixer)
     g.positions = nodes
     g.radius = radius
     g.B = B
@@ -449,7 +649,7 @@ def unit_disk_grid_graph_rydberg(grid, radius=np.sqrt(2) + 1e-5, B=863300 / (4.4
 
 
 def rydberg_graph(points, B=863300 / 4.47 ** 6, alpha=6, threshold=1e-8, label_node_by_coords=False,
-                  visualize=False, IS=True, periodic=False):
+                  visualize=False, IS=True, periodic=False, generate_mixer=False):
     """ Create a Graph object from xy-coordinates
     where the edge weights correspond to
             B / ||r_i - r_j||^alpha
@@ -503,14 +703,14 @@ def rydberg_graph(points, B=863300 / 4.47 ** 6, alpha=6, threshold=1e-8, label_n
         nx.draw_networkx(graph, pos=pos,
                          labels={}, node_size=20, node_color='r')
         plt.show()
-    graph = Graph(graph, IS=IS)
+    graph = Graph(graph, IS=IS, generate_mixer=generate_mixer)
     graph.positions = points
     graph.B = B
     graph.periodic = periodic
     return graph
 
 
-def unit_disk_graph(points, radius=1 + 1e-5, periodic=False, visualize=False, IS=True):
+def unit_disk_graph(points, radius=1 + 1e-5, periodic=False, visualize=False, IS=True, generate_mixer=False):
     nodes = np.arange(points.shape[0])
     adjacency_matrix = np.zeros((points.shape[0], points.shape[0]))
     for n1 in nodes:
@@ -524,12 +724,12 @@ def unit_disk_graph(points, radius=1 + 1e-5, periodic=False, visualize=False, IS
         pos = {nodes[i]: points[i] for i in range(len(nodes))}
         nx.draw(graph, pos=pos)
         plt.show()
-    graph = Graph(graph, IS=IS)
+    graph = Graph(graph, IS=IS, generate_mixer=generate_mixer)
     graph.positions = nodes
     return graph
 
 
-def branching_tree_from_edge(n_branches, visualize=True, IS=True):
+def branching_tree_from_edge(n_branches, visualize=True, IS=True, generate_mixer=False):
     graph = nx.Graph()
     graph.add_edge(0, 1)
     last_layer = [0, 1]
@@ -545,4 +745,4 @@ def branching_tree_from_edge(n_branches, visualize=True, IS=True):
     if visualize:
         nx.draw(graph, with_labels=True)
         plt.show()
-    return Graph(graph, IS=IS)
+    return Graph(graph, IS=IS, generate_mixer=generate_mixer)

qsim/schrodinger_equation.py
@@ -76,7 +76,7 @@ class SchrodingerEquation(object):
                 return z, infodict
             else:
                 if times is None:
-                    times = np.linspace(int(t0), int(tf), num=int(num))
+                    times = np.linspace(t0, tf, num=int(num))
                 norms = np.zeros(len(times))
                 s = state_asarray.copy()
                 for (i, t) in zip(range(len(times)), times):

qsim/scripts/heisenberg_mat_exp.py
@@ -51,23 +51,19 @@ def chebyshev(A, v, tau, eps, p_check, return_cost=False):
         return bessels
 
     vnorm = np.linalg.norm(v)
-    print(vnorm)
 
     def m_max(eps, tau, lambda_1, lambda_n):
         bound = eps / (4 * vnorm)
-        print(vnorm, eps)
         if ((0.5) ** (np.exp(1) * tau * (lambda_n - lambda_1) / 2) < bound):
             return int(np.exp(1) * tau * (lambda_n - lambda_1) / 2)
         else:
-            print(bound)
-            print(np.log2(1 / bound))
             return int(np.log(1 / bound) / np.log(2))
 
     # Compute conservative upper bound for the number of terms needed
     m_upper = m_max(eps, tau, lambda_1, lambda_n)
-    print(m_upper)
+    print('m_upper', m_upper)
     chebs_upper = chebs(m_upper, l1, ln, tau)
-    print(chebs_upper)
+    #print(chebs_upper)
     # initialize vectors etc for cheb iteration
     v_old = v
     v_new = matvec(A, v) / l1 - ln / l1 * v
@@ -92,6 +88,7 @@ def chebyshev(A, v, tau, eps, p_check, return_cost=False):
                 m_upper = truncation_m(k0, eps, s_norms[k + 1 - p_check], v)
         k += 1
         exit_test = (k == m_upper)
+        #print(m_upper)
         v_new, v_old = v_old, v_new
     if return_cost:
         runtime = time.time() - t0
@@ -244,13 +241,53 @@ def time_evolve(tau, eps, L=12, h=1, verbose=False):
     print('done cheb, {} matvec, {} s'.format(nmatvec, runtime))
 
 
+def p_check_compare(tau, eps, L=12, h=1, verbose=False):
+    if verbose:
+        print('starting ham')
+    disorder = (np.random.random(size=L) - 1 / 2) * h
+    #disorder = np.array([0.848539, -0.038314, -0.170679, 0.384688, 0.00254869, 0.877209, -0.523275, -0.628435])/2
+    #np.set_printoptions(threshold=np.inf)
+    #print(repr(disorder))
+    graph = line_graph(L)
+    Heis = ham.HamiltonianHeisenberg(graph, energies=(1/4, 1/4), subspace='all')
+
+    def matvec_disorder(x):
+        if len(x.shape) == 2:
+            return matvec_heisenberg(Heis, disorder, State(x))
+        else:
+            return matvec_heisenberg(Heis, disorder, State(np.expand_dims(x, axis=-1)))
+
+    disorder_heis = sp.sparse.linalg.LinearOperator((2 ** L, 2 ** L), matvec=matvec_disorder)
+    dill.dump(disorder_heis, open('heisenberg_linear_op_' + str(graph.n) + '.pickle', 'wb'))
+    if verbose:
+        print('done with ham')
+    # initial state:
+    v0 = State(np.zeros((2 ** L, 1)) )
+    v0[0]=1
+    p_checks = np.arange(1, 11)
+    #nu_max = 7
+    #if verbose:
+    #    print('start kryl')
+    #nmatvec, runtime, expk = krylov(disorder_heis, v0, tau, eps, p_check, nu_max, return_cost=True)
+    #print('done kryl, {} matvec, {} s'.format(nmatvec, runtime))
+    tols = [1e-2, 1e-4, 1e-6]
+    matvecs = np.zeros((len(tols), len(p_checks)))
+    for (t, tol) in enumerate(tols):
+        for (p, p_check) in enumerate(p_checks):
+            if verbose:
+                print('start cheb')
+            nmatvec, runtime, expc = chebyshev(disorder_heis, v0, tau, tol, p_check, return_cost=True)
+            matvecs[t, p] = nmatvec
+            print('done cheb, {} matvec, {} s, {}'.format(nmatvec, runtime, p_check))
+            print(matvecs)
+
+
 # print('diff', np.linalg.norm(expc-expk)/np.linalg.norm(expc))
 # expe = sp.sparse.linalg.expm_multiply(-1j*tau*disorder_heis, v0.squeeze())
 # print('done scipy', expe.shape)
 # print(np.max(np.abs((expc-expe)/expe)))
 # print(np.max(np.abs((expk-expe)/expe)))
-n = 8
-print(2 ** n)
+n = 14
 #taus = 10 ** np.linspace(-2, 0, 3)
 #tols = 10 ** np.linspace(-8, -2, 10)
-time_evolve(1e0, 1e-5, L=n, verbose=True)
+p_check_compare(10, 1e-2, L=n, verbose=True)

qsim/scripts/mat_exp_practice.py
@@ -435,5 +435,5 @@ def return_probability(graph, times, verbose=False, h=1, exact=True):
     return czz_tot / num
 
 
-print(10 ** np.linspace(0, 3, 5))
-print(np.mean(return_probability(line_graph(6), np.concatenate([[0], 10 ** np.linspace(0, 3, 10)]), h=1), axis=1))
+#print(10 ** np.linspace(0, 3, 5))
+#print(np.mean(return_probability(line_graph(6), np.concatenate([[0], 10 ** np.linspace(0, 3, 10)]), h=1), axis=1))

qsim/scripts/performance_small_graphs.py
@@ -10,6 +10,14 @@ import scipy.optimize
 import pandas as pd
 
 
+def cost_reduction():
+    pass
+
+
+def cost_addition():
+    pass
+
+
 def loadfile(graph_index, size):
     graph_mask = np.reshape(np.loadtxt("mis_degeneracy_L%d.dat" % size)[graph_index, 3:],
                             (size, size), order='F')[::-1, ::-1].T.astype(bool)
@@ -23,8 +31,10 @@ def loadfile(graph_index, size):
 
 def find_ratio(tails_graph, graph, tf):
     cost = hamiltonian.HamiltonianMIS(graph, IS_subspace=True)
+    print('Starting driver')
     driver = hamiltonian.HamiltonianDriver(IS_subspace=True, graph=graph)
-    rydberg = hamiltonian.HamiltonianRydberg(tails_graph, graph, IS_subspace=True, energies=(1,))
+    print('Starting rydberg')
+    rydberg = hamiltonian.HamiltonianRydberg(tails_graph, graph, IS_subspace=True, energies=(2*np.pi,))
 
     def schedule(t, T):
         # Linear ramp on the detuning, experiment-like ramp on the driver
@@ -36,9 +46,26 @@ def find_ratio(tails_graph, graph, tf):
                             -1 / (1 + np.e ** (k * (x - a))) ** b - 1 / (1 + np.e ** (-k * (x - (1 - a)))) ** b + 1) / \
                     (-1 / ((1 + np.e ** (k * (1 / 2 - a))) ** b) - 1 / (
                             (1 + np.e ** (-k * (1 / 2 - (1 - a)))) ** b) + 1)
-        cost.energies = (11 * 2 * (1 / 2 - x),)
-        driver.energies = (4 * amplitude,)
+        cost.energies = (2 * np.pi * (-(11 + 15) / T * t + 15),)
+        driver.energies = (2 * np.pi * 2 * amplitude,)
 
+
+    def schedule_exp_optimized(t, T, pulse=None):
+        T=T-2*0.311
+        driver.energies = (2 * np.pi * np.interp(t, pulse[:, 0], pulse[:, 1] / 2),)
+        cost.energies = (2 * np.pi * np.interp(t, pulse[:, 0], -pulse[:, 2]),)
+
+    def schedule_exp_linear(t, T):
+        if t < .311:
+            driver.energies = (2 * np.pi * 2 * t / .311,)
+            cost.energies = (2 * np.pi * 15,)
+        elif .311 <= t <= T - .331:
+            driver.energies = (2 * np.pi * 2,)
+            cost.energies = (2 * np.pi * (-(11 + 15) / (T - 2 * .311) * (t - .311) + 15),)
+        else:
+            driver.energies = (2 * np.pi * 2 * (T - t) / .311,)
+            cost.energies = (-2 * np.pi * 11,)
+        # print(t, cost.energies)
     # Uncomment this to print the schedule at t=0
     # schedule(0, 1)
     # print(cost.hamiltonian*2*np.pi)
@@ -46,24 +73,32 @@ def find_ratio(tails_graph, graph, tf):
 
     ad = SimulateAdiabatic(graph=graph, hamiltonian=[cost, driver, rydberg], cost_hamiltonian=cost,
                            IS_subspace=True)
-
-    final_state = ad.run(tf, schedule, method='odeint')[0][-1]
-    cost.energies = (1,)
-    ar = cost.approximation_ratio(final_state)
-    return ar
+    print('Starting evolution')
+    ars = []
+    for i in range(len(tf)):
+        states, data = ad.run(tf[i], schedule_exp_linear, num=int(200 * tf[i]), method='trotterize', full_output=False)
+        cost.energies = (1,)
+        ar = cost.approximation_ratio(states[-1])
+        print(i, tf[i], ar)
+        ars.append(ar)
+    return ars
 
 
 def visualize_low_energy_subspace(graph, k=5):
+    print('initializing')
     cost = hamiltonian.HamiltonianMIS(graph, IS_subspace=True)
+    print('finished MIS')
     driver = hamiltonian.HamiltonianDriver(IS_subspace=True, graph=graph)
+    print('finished driver')
     # rydberg = hamiltonian.HamiltonianRydberg(tails_graph, graph, IS_subspace=True, energies=(1,))
     grid = graph.positions
-    random = hamiltonian.HamiltonianEnergyShift(IS_subspace=True, graph=graph, energies=(1e-6,))
-    random._hamiltonian = scipy.sparse.csc_matrix(
-        (np.random.normal(loc=1e3, scale=1e3, size=graph.num_independent_sets), (np.arange(graph.num_independent_sets),
-                                                                                 np.arange(
-                                                                                     graph.num_independent_sets))),
-        shape=(graph.num_independent_sets, graph.num_independent_sets))
+
+    # random = hamiltonian.HamiltonianEnergyShift(IS_subspace=True, graph=graph, energies=(1e-6,))
+    # random._hamiltonian = scipy.sparse.csc_matrix(
+    #    (np.random.normal(loc=1e3, scale=1e3, size=graph.num_independent_sets), (np.arange(graph.num_independent_sets),
+    #                                                                             np.arange(
+    #                                                                                 graph.num_independent_sets))),
+    #    shape=(graph.num_independent_sets, graph.num_independent_sets))
 
     def schedule(t, T):
         # Linear ramp on the detuning, experiment-like ramp on the driver
@@ -87,15 +122,17 @@ def visualize_low_energy_subspace(graph, k=5):
     # schedule(0, 1)
     # print(cost.hamiltonian*2*np.pi)
     # print(driver.hamiltonian)
-    eq = schrodinger_equation.SchrodingerEquation(hamiltonians=[cost, driver, random])
+    eq = schrodinger_equation.SchrodingerEquation(hamiltonians=[cost, driver])
     fig = plt.figure(tight_layout=True)
     k_cutoff = 5
     gs = gridspec.GridSpec(k_cutoff, k_cutoff)
     ax = fig.add_subplot(gs[:, 0:k_cutoff - 1])
-    num = 200
-    for (i, t) in enumerate(np.linspace(.05, .95, num)):
+    num = 50
+    print('beginning computation')
+    for (i, t) in enumerate(np.linspace(.5, .95, num)):
         print(i)
         g, eigvec = gap(t)
+        print(g)
         if i == num - 1:
             probs = np.abs(eigvec) ** 2
             for l in range(k_cutoff):
@@ -136,13 +173,14 @@ def find_gap(graph, k=2):
                             -1 / (1 + np.e ** (k * (x - a))) ** b - 1 / (1 + np.e ** (-k * (x - (1 - a)))) ** b + 1) / \
                     (-1 / ((1 + np.e ** (k * (1 / 2 - a))) ** b) - 1 / (
                             (1 + np.e ** (-k * (1 / 2 - (1 - a)))) ** b) + 1)
-        cost.energies = (5 * 2 * (1 / 2 - x),)
-        driver.energies = (amplitude,)
+        cost.energies = (11 * 2 * (1 / 2 - x),)
+        driver.energies = (2*amplitude,)
 
     def gap(t):
         t = t[0]
         schedule(t, 1)
         eigval, eigvec = eq.eig(which='S', k=k)
+        print(t, np.abs(eigval[1] - eigval[0]))
         return np.abs(eigval[1] - eigval[0])
 
     # Uncomment this to print the schedule at t=0
@@ -151,89 +189,70 @@ def find_gap(graph, k=2):
     # print(driver.hamiltonian)
     eq = schrodinger_equation.SchrodingerEquation(hamiltonians=[cost, driver, random])
     # Do minimization
-    return scipy.optimize.minimize(gap, np.array([.63]), bounds=[(.5, .68)], tol=1e-2).fun
+    res = scipy.optimize.minimize(gap, np.array([.645]), bounds=[(.55, .7)], tol=1e-2)
+    return res.fun, res.x[0]
 
 
-"""gaps = np.array([0.5020390838524253, 0.49406980906189446, 0.5578671193299023, 0.5682452892269829, 0.5564839733350588, 0.5551982927732428, 0.6197667567917655, 0.7096368857589059, 0.5525375459495105, 0.7309634399792699, 0.6890373439682342, 0.7050813597692294, 0.7051066962851582, 0.7146592090624981, 0.7250230280073069, 0.6786498194974975, 0.6625055636487698, 0.8785021342453643, 0.8145767848451175, 0.7038690448021434, 0.8784281926118798, 0.7206137688950331, 0.841583058127604, 0.7430021851145288, 0.7425451480430034, 0.7413106249240862, 0.7408932196924596, 0.5977275471880112, 0.8275948725226865, 0.6852943520829111, 0.8393314782629524, 0.8400803778070784, 0.8138045171292774, 0.7911154114349053, 0.8118002009920655, 0.6852374553005998, 0.8276171617588712, 0.8276685056095321, 0.6348678430839101, 0.6968779721318974, 0.715008637535222, 0.7336275320361825, 0.8072057132808244, 0.910128784719733, 0.7124284365965838, 0.712437896131954, 1.1328240737616717, 1.1418965577886855, 0.7993776021279224, 0.7827528712412537, 1.1408664131914765, 0.8144205107611171, 0.9868511192092964, 1.1416286234877937, 0.8036866647687511, 1.1394514075015056, 0.7806245763275967, 0.7571833189473764, 0.8362776429995513, 0.7867164462752037, 0.6360074928540236, 0.7356544085473153, 0.762529887615834, 0.8891864427643448, 1.140899044112249, 0.97476336746616, 0.8142365432286596, 1.149107884599852, 0.876712000417891, 0.7132383792767065, 1.129714243497144, 1.131473517173795, 0.7082766356957926, 0.7812759737940862, 0.8845863631400821, 0.7580294063116835, 1.1297322610287228, 1.1384763868430774, 0.7556774817338123, 0.7748748428642287, 1.0880981169523363, 0.9789711098750313, 1.1386336402337456, 1.1355791904849966, 1.1470671012034792, 0.944217878233351, 1.0959774739975732, 1.141808141117144, 1.1096187071615713, 1.0758793841547458, 1.1465369875301743, 0.7586312958530161, 1.1388819898113525, 0.7632140928767601, 1.0498850610703414, 0.773831768108332, 1.1096207964367046, 0.7586133421787125, 1.1452288103942099, 1.1100221019536018])
-ratios = np.array([45.5, 43.5, 36.0, 36.0, 34.0, 34.0, 33.0, 32.0, 32.0, 30.0, 30.0, 29.5, 29.5, 29.0, 28.0, 28.0, 28.0, 27.0, 27.0, 27.0, 27.0, 27.0, 27.0, 26.0, 26.0, 26.0, 26.0, 25.5, 25.0, 25.0, 25.0, 25.0, 25.0, 25.0, 25.0, 25.0, 25.0, 25.0, 24.666666666666668, 24.5, 24.0, 24.0, 24.0, 24.0, 24.0, 24.0, 23.0, 23.0, 23.0, 23.0, 23.0, 23.0, 23.0, 23.0, 23.0, 23.0, 22.5, 22.5, 22.5, 22.5, 22.333333333333332, 22.25, 22.0, 22.0, 22.0, 22.0, 22.0, 22.0, 22.0, 22.0, 22.0, 22.0, 22.0, 22.0, 22.0, 22.0, 22.0, 22.0, 21.8, 21.5, 21.0, 21.0, 21.0, 21.0, 21.0, 21.0, 21.0, 21.0, 21.0, 21.0, 21.0, 21.0, 21.0, 21.0, 21.0, 21.0, 21.0, 21.0, 21.0, 21.0])
-plt.scatter(1/ratios, gaps,color='cornflowerblue')
-plt.ylabel(r'adiabatic gap ($\Omega_{\max} = 1$)')
-plt.xlabel(r'(# MIS)/(# MIS-1)')
-plt.show()"""
+def collect_gap_statistics(size):
+    size_indices = np.array([5, 6, 7, 8, 9, 10])
+    size_index = np.argwhere(size == size_indices)[0, 0]
+    gaps = []
+    ratios = []
+    degeneracies = []
+    for index in range(100):
+        xls = pd.ExcelFile('MIS_degeneracy_ratio.xlsx')
+        """graphs = np.array([667, 557,  78, 312, 807, 776, 485, 980,  71,  50, 521, 773, 549,
+           523, 374, 515, 669, 344,  21, 107, 201, 851, 736, 508, 286, 526,
+           385, 116,  20, 999, 357, 149, 872, 233, 528, 603, 912, 820, 155,
+           744, 438, 931,  68, 610, 209, 876, 558, 809, 702, 194, 828, 437,
+           470, 958, 359, 677, 185, 813, 715, 420, 153, 573, 394, 542, 688,
+           863, 771, 325, 502, 795, 617, 722, 793, 182, 363, 984, 447, 506,
+           673, 950, 329, 127, 492, 428, 343, 391, 812, 949,  69, 265, 276,
+           564, 336, 966, 963, 219, 321, 144, 435, 696])"""
 
-size = 6
-size_indices = np.array([5, 6, 7, 8, 9, 10])
-size_index = np.argwhere(size == size_indices)[0, 0]
-#gaps = []
-#ratios = []
+        # graph_index = graphs[index]
+        graph_index = int(pd.read_excel(xls, 'Sheet1').to_numpy()[index, size_index])
+
+        graph_data = loadfile(graph_index, size)
+        grid = graph_data['graph_mask']
+        # We want to have the file of hardest graphs, then import them
+        graph = unit_disk_grid_graph(grid, periodic=False, generate_mixer=True)
+        raise Exception
+        # print(np.arange(graph.num_independent_sets, 0, -1))
+        # print(np.sum(2 ** np.arange(graph.n, 0, -1) * (1 - graph.independent_sets), axis=1))
+        # print(1-graph.independent_sets)
+        print('Degeneracy', graph.degeneracy)
+        print('MIS size', graph.mis_size)
+        print('Hilbert space size', graph.num_independent_sets)
+        # tails_graph = rydberg_graph(grid, visualize=False)
+        gap, loc = find_gap(graph, k=2)
+        gaps.append(gap)
+        degeneracies.append(graph.degeneracy)
+        is_sizes = np.sum(1 - graph.independent_sets, axis=1)
+        ratio = np.sum(is_sizes == graph.mis_size - 1) / np.sum(is_sizes == graph.mis_size)
+        ratios.append(ratio)
+        print(gaps, degeneracies)
+
+        # print(index, graph_index, gap, loc, ratio, graph.degeneracy)
+        # visualize_low_energy_subspace(graph)
+
+
 if __name__ == '__main__':
-    import sys
-    index = int(sys.argv[1])
+    # Evolve
+    # import sys
+    # index = int(sys.argv[1])
+    index = 0
+    size = 7
+    size_indices = np.array([5, 6, 7, 8, 9, 10])
+    size_index = np.argwhere(size == size_indices)[0, 0]
     xls = pd.ExcelFile('MIS_degeneracy_ratio.xlsx')
     graph_index = int(pd.read_excel(xls, 'Sheet1').to_numpy()[index, size_index])
+    print(graph_index,)
+
     graph_data = loadfile(graph_index, size)
     grid = graph_data['graph_mask']
-    # We want to have the file of hardest graphs, then import them
-    graph = unit_disk_grid_graph(grid, periodic=False)
-    # print(np.arange(graph.num_independent_sets, 0, -1))
-    # print(np.sum(2 ** np.arange(graph.n, 0, -1) * (1 - graph.independent_sets), axis=1))
-    # print(1-graph.independent_sets)
-    #print('Degeneracy', graph.degeneracy)
-    #print('MIS size', graph.mis_size)
-    #print('Hilbert space size', graph.num_independent_sets)
-    tails_graph = rydberg_graph(grid, visualize=False)
-    gap = find_gap(graph, k=2)
-    #gaps.append(gap)
-    is_sizes = np.sum(1 - graph.independent_sets, axis=1)
-    ratio = np.sum(is_sizes == graph.mis_size - 1) / np.sum(is_sizes == graph.mis_size)
-    #ratios.append(ratio)
-    print(gap, ratio, graph.degeneracy)
-"""# Evolve
-times = 2 * np.pi * 2 ** np.linspace(-2.5, 2, 7)
-ratios = []
-
-for time in times:
-    print('Time', time)
-    ratio = find_ratio(graph, time)
-    print('Ratio', 1 - ratio)
-    ratios.append(ratio)
-    plt.scatter(time / 2 / np.pi, 1 - ratio, color='navy')
-
-print(1 - np.array(ratios))
-plt.loglog()
-plt.ylabel(r'1-ratio')
-plt.xlabel(r'total time ($\mu s$)')
-plt.title(r'$5\times 5$ graphs, $r=1.50$ with no additions')
-plt.show()
-
-plt.clf()
-# Results for time series
-times = 2 * np.pi * np.linspace(.1, 4, 20)
-ratios_410 = 1 - np.array(
-    [0.6312698400365628, 0.6350631502841703, 0.7363947755015506, 0.7662979536725513, 0.8167691849746471,
-     0.8623958274791159, 0.8913939185975078, 0.912168628992816, 0.9321780785078123, 0.9462223416029758,
-     0.9565127845162442, 0.9662627545199562, 0.9728921938205923, 0.9781659289712348, 0.9827158152246354,
-     0.9860167897451267, 0.9887534729034296, 0.9909437382177713, 0.9926390836244009, 0.9940074711333567])
-plt.plot(times / 2 / np.pi, ratios_410, color='navy', label='410')
-plt.scatter(times / 2 / np.pi, ratios_410, color='navy')
-ratios_407 = 1 - np.array(
-    [0.630063776878089, 0.6416610996304736, 0.7444381410820011, 0.7829055244953346, 0.8353697195102272,
-     0.8817360513054348, 0.9132642942385995, 0.92883017789128, 0.9477625711705338, 0.9610277728044203,
-     0.9699514991171989, 0.9775476589769881, 0.9833353927192476, 0.987173459261333, 0.9904535712504051,
-     0.9927588688193951, 0.9944782876829874, 0.9958044215213879, 0.9967836134836626, 0.9975004790785146])
-plt.plot(times / 2 / np.pi, ratios_407, color='cornflowerblue', label='407')
-plt.scatter(times / 2 / np.pi, ratios_407, color='cornflowerblue')
-ratios_396 = 1 - np.array(
-    [0.570659624797462, 0.6308651114758622, 0.7424121339736333, 0.7870918781716714, 0.8284875558347364,
-     0.8765767421749913, 0.89789528347941, 0.9269253219093562, 0.9443487385154993, 0.9568907893731936,
-     0.9683788011764236, 0.9760734973226479, 0.9822823276485927, 0.9869070815629714, 0.990429418338397,
-     0.9930128114995059, 0.9949157145006375, 0.9963084836091451, 0.9973188688684352, 0.9980537941090958])
-plt.plot(times / 2 / np.pi, ratios_396, color='teal', label='396')
-plt.scatter(times / 2 / np.pi, ratios_396, color='teal')
-plt.legend()
-plt.ylabel(r'1-ratio')
-plt.xlabel(r'total time ($\mu s$)')
-plt.title(r'$5\times 5$ graphs, $r=1.50$ with no additions')
-plt.loglog()
-plt.show()
-"""
+    graph = unit_disk_grid_graph(grid, periodic=False, visualize=False, generate_mixer=True)
+    tails_graph = rydberg_graph(grid)
+    tf = (2**np.linspace(-2.5, 2, 7)+2*.311)#*(2*np.pi)
+    ratio = find_ratio(tails_graph, graph, tf)
+    print(ratio)